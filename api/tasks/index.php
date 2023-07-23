1bc836566044add2df83652b21bd99e2f0bdecaf
MonicaIzzo <info@monicaizzo.net>
2023-07-24 00:02:30 +0200

    step 2

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -1,15 +1,30 @@
 <?php
 
-// Dati di base
-
+// Recupero l'indirizzo del file che simula il database
 $database_path = __DIR__ . '/../../database/tasks.json';
 
+// leggo il contenuto di Json
 $json_data = file_get_contents($database_path);
 
+// lo converto in un array
 $tasks = json_decode($json_data, true);
 
-// Avviso che la risposta è in json
-header('Content-Type: application/json');
+// Controlliamo se abbiamo qualcosa in post
+$new_task = $_POST['task'] ?? null;
+if ($new_task) {
+    $tasks[] = $new_task;
 
-//Converto in Json
-echo json_encode($tasks);
+    $json_tasks = json_encode($tasks);
+    file_put_contents($database_path, $json_tasks);
+
+    // Avviso chi mi riceve che la risposta è risposta è in json
+    header('Content-Type: application/json');
+
+    echo $new_task;
+} else {
+    // Avviso chi mi riceve che la risposta è risposta è in json
+    header('Content-Type: application/json');
+
+    //Converto in Json
+    echo json_encode($tasks);
+}
--- my_app/js/script.js
+++ my_app/js/script.js
@@ -3,17 +3,32 @@ const { createApp} = Vue;
 const app = createApp({
     data(){
         return {
-            tasks: []
+            tasks: [],
+            newTask: '',
         } 
+    },
+    methods: {
+        addTask() {
+            // preparo i dati
+            const data = {task : this.newTask};
+
+            //preparo le configurazioni
+            const config = {headers: {'Content-Type' : 'multipart/form-data'}}
+
+            axios.post('http://localhost:8888/php-todo-list-json/api/tasks/', data, config)
+            .then(res => {
+                this.tasks.push(res.data);
+                this.newTask = '';
+            });
+
+        }
     },
     created() {
         axios.get('http://localhost:8888/php-todo-list-json/api/tasks/')
         .then(res => {
             this.tasks = res.data;
         });
-
     }
-
 })
 
 app.mount('#app')
